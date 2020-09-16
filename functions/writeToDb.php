a67f954405a51355508f7a865d4debf7bf057797
Alexandr <golinov95@gmail.com>
2020-09-16 14:36:29 +0300

    update

--- functions/writeToDb.php
+++ functions/writeToDb.php
@@ -7,27 +7,31 @@ function writeToDb($data)
     $dbh = getDbCon();
     $dbh->beginTransaction();
     try {
-        $checkQuestion = $dbh->prepare('SELECT question_id FROM questions WHERE question = :question');
-        $checkQuestion->bindParam(':question', $data['question']);
-        $checkQuestion->execute();
+        $checkQuestion = $dbh->prepare('SELECT question_id FROM questions WHERE question = ?');
+        $checkQuestion->execute([
+            $data['question']
+        ]);
         $questionResult = $checkQuestion->fetchColumn();
         if (!$questionResult) {
-            $question = $dbh->prepare('INSERT INTO questions(question) VALUES (:question)');
-            $question->bindParam(':question', $data['question']);
-            $question->execute();
+            $question = $dbh->prepare('INSERT INTO questions(question) VALUES (?)');
+            $question->execute([
+                $data['question']
+            ]);
             $question_id = $dbh->lastInsertId();
         } else $question_id = $questionResult;
 
-        $checkAnswer = $dbh->prepare('SELECT answer_id FROM answers WHERE answer = :answer');
-        $answer = $dbh->prepare('INSERT INTO answers(answer,length) VALUES(:answer, :length)');
+        $checkAnswer = $dbh->prepare('SELECT answer_id FROM answers WHERE answer = ?');
+        $answer = $dbh->prepare('INSERT INTO answers(answer,length) VALUES(?, ?)');
         foreach ($data['answer'] as $key => $value) {
-            $checkAnswer->bindParam(':answer', $value);
-            $checkAnswer->execute();
+            $checkAnswer->execute([
+                $value
+            ]);
             $answer_id[$key] = $checkAnswer->fetchColumn() ?? null;
             if (empty($answer_id[$key])) {
-                $answer->bindParam(':answer', $value);
-                $answer->bindParam(':length', $data['length'][$key]);
-                $answer->execute();
+                $answer->execute([
+                    $value,
+                    $data['length'][$key]
+                ]);
                 $answer_id[$key] = $dbh->lastInsertId();
             }
         }
